b5b24ed9084b9b2d268bbe835231dc78ee258753
Diana Baburina <dbaburina.consultant@newscorp.com>
2021-02-01 15:07:54 +0200

    Request articles

--- locust-scripts/locustfile.py
+++ locust-scripts/locustfile.py
@@ -4,12 +4,36 @@ from urllib3 import disable_warnings
 
 from locust import HttpLocust, TaskSet, task
 
+def extract_data(data, path_fragments):
+    child_path = path_fragments.pop(0)
+    is_array = child_path.endswith('[]')
+
+    if is_array: child_path = child_path[:len(child_path) - 2]
+    if not child_path in data: return []
+    
+    child_data = data[child_path]
+    if (child_data is None): return []
+    
+    if len(path_fragments) == 0:
+        if (isinstance(child_data, str)): return [child_data]
+        if (isinstance(child_data, list)): return child_data
+        return [child_data]
+        
+    if (is_array and isinstance(child_data, list)):
+        results = []
+        for item in child_data:
+            results.extend(extract_data(item, path_fragments[:]))
+        return results
+        
+    return extract_data(child_data, path_fragments)
+
 class UserBehavior(TaskSet):
     TIMEOUT_SECONDS = 60
 
     APPLICATIONS = ['dailytelegraph']
     SECTIONS = dict()
     ARTICLES = dict()
+    MAX_SECTIONS_FOR_ARTICLES_REQUEST = 10
 
     def get_random_application(self):
         return random.choice(self.APPLICATIONS)
@@ -30,6 +54,18 @@ class UserBehavior(TaskSet):
                 if theater_id.endswith('--collection') and not theater_id == 'top-stories--collection':
                     self.SECTIONS[application] = self.SECTIONS.get(application, []) + [theater_id]
 
+    def set_articles(self):
+        for _ in range(0, self.MAX_SECTIONS_FOR_ARTICLES_REQUEST):
+            application = self.get_random_application()
+            section = self.get_random_item_from_dict(self.SECTIONS, application)
+            
+            section_response = self.client.get('/apps/' + application + '/theaters/' + section, headers=self._headers, timeout=self.TIMEOUT_SECONDS, verify=False).json()
+            frames = extract_data(section_response, 'screens[].frames[]'.split('.'))
+
+            for frame in frames:
+                if frame['type'] == 'article':
+                    self.ARTICLES[application] = self.ARTICLES.get(application, []) + [{ 'theater_id': frame['theaterId'], 'article_id': frame['articleId'] }]
+
     def on_start(self):
         """Called when a Locust start before any task is scheduled"""
         self.client.verify = False
@@ -38,6 +74,7 @@ class UserBehavior(TaskSet):
             self._headers['x-access-token'] = environ['X_ACCESS_TOKEN']
 
         self.set_sections()
+        self.set_articles()
 
     @task(1)
     def app_task1_root(self):
@@ -48,10 +85,16 @@ class UserBehavior(TaskSet):
         self.client.get('/apps/' + self.get_random_application() + '/theaters/top-stories', headers=self._headers, timeout=self.TIMEOUT_SECONDS, verify=False)
 
     @task(1)
-    def app_task2_sections(self):
+    def app_task3_sections(self):
         application = self.get_random_application()
         section = self.get_random_item_from_dict(self.SECTIONS, application)
         self.client.get('/apps/' + application + '/theaters/' + section, headers=self._headers, timeout=self.TIMEOUT_SECONDS, verify=False)
+
+    @task(1)
+    def app_task4_articles(self):
+        application = self.get_random_application()
+        article = self.get_random_item_from_dict(self.ARTICLES, application)
+        self.client.get('/apps/' + application + '/theaters/' + article['theater_id'] + '?screen_ids=' + article['article_id'], headers=self._headers, timeout=self.TIMEOUT_SECONDS, verify=False)
 
 class WebsiteUser(HttpLocust):
     task_set = UserBehavior
